b56d8c82efa2f538f3826e06d2d1c74919f1c518
Ali Polanunu <154661557+alii16@users.noreply.github.com>
2025-06-02 19:35:01 +0800

    Update Constants.java

--- app/src/main/java/com/example/pointnews/util/Constants.java
+++ app/src/main/java/com/example/pointnews/util/Constants.java
@@ -1,6 +1,6 @@
 package com.example.pointnews.util;
 
 public class Constants {
-    public static final String API_KEY = "06d86443554b4963900a62d467f94394";
+    public static final String API_KEY = "YOUR_API_KEY";
     public static final String COUNTRY_CODE = "us";
 }
