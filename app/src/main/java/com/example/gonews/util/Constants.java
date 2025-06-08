c1e04645dc7258b9abea0550a73aa5ab0fb464e3
Ali Polanunu <154661557+alii16@users.noreply.github.com>
2025-06-08 22:55:49 +0800

    Update Constants.java

--- app/src/main/java/com/example/gonews/util/Constants.java
+++ app/src/main/java/com/example/gonews/util/Constants.java
@@ -1,7 +1,7 @@
 package com.example.gonews.util; // Atau com.example.gonews.constants
 
 public class Constants {
-    public static final String API_KEY = "pub_111cd9df5da140209b354e85236dc87b"; // API Key NewsData.io Anda
+    public static final String API_KEY = "YOUR_API_KEY"; // API Key NewsData.io Anda
     public static final String COUNTRY_CODE = "id"; // Mengubah kode negara ke Indonesia
     public static final String LANGUAGE_CODE = "id"; // Menambahkan kode bahasa untuk Indonesia
 }
